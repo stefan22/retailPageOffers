8f64115d719c0aa055e380c86652a0f3330aaae2
Stefano <stefano.lissiaeu@gmail.com>
2017-05-01 04:09:52 +0100

    desktop completed

--- js/script.js
+++ js/script.js
@@ -46,6 +46,7 @@ externalScripts();
         this.doGrid();
         this.newFoot();
         this.quidFoot();
+        this.footRemodel();
      
         
      
@@ -55,7 +56,7 @@ externalScripts();
 
     pgCssDesktop: function() {
         console.info('%c pgCssMobile \u221a','background:blue;color:white;');
-        var mainCss = '#grid {width:100%;padding:0; } .responsive-component-body {margin-top:30px;} #browse_footer {margin-top:0;} .linea {margin-top:0;border-top: 1px dotted rgba(211, 210, 210, 0.25);} .component1 a {color:#d3d2d2;} div#netotiate-hub-container {background-color:#231f20;} .component2 a {color:#d3d2d2;} .component3 a {color:#d3d2d2;} div#qfoot.logo.quidfoot a svg.main-logo {fill:white; margin-left:0;} #quidtext {font-size:14px; color:#fff; margin:1em 0 0 0; letter-spacing:1px; text-align:center;} #qfoot {padding:30px 0; margin:0 auto; text-align:center; background-color:#4f4c4c;} ';
+        var mainCss = '#grid {width:100%;padding:0; } .responsive-component-body {margin-top:30px;} #browse_footer {margin-top:0;} .linea {margin-top:0;border-top: 1px dotted rgba(211, 210, 210, 0.25);width:90%;margin-left:20px;} .component1 a {color:#d3d2d2;} div#netotiate-hub-container {background-color:#231f20;} .component2 a {color:#d3d2d2;} .component3 a {color:#d3d2d2;} div#qfoot.logo.quidfoot a svg.main-logo {fill:white; margin-left:0;} #quidtext {font-size:14px; color:#fff; margin:1em 0 0 0; letter-spacing:1px; text-align:center;} #qfoot {padding:30px 0; margin:0 auto; text-align:center; background-color:#4f4c4c;} .responsive-component-body1 {margin-top:10px;} ';
         var head = document.getElementsByTagName('head')[0];
         function addcss(css) {
             var s = document.createElement('style');
@@ -163,12 +164,24 @@ externalScripts();
         div.className = "row";
         div.innerHTML = ' <div id="qfoot" class="logo quidfoot"> <a href="/" style="outline: 0px;"> <svg class="main-logo"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-logo"></use> </svg> </a><p id="quidtext">Rewarding shopping everyday!</p></div> ';
 
-        var contar = document.querySelectorAll('.container')[12];
+        var contar = document.querySelectorAll('.container')[11];
         var par = contar.parentElement;
         par.insertBefore(div,contar);
 
 
-    }//quidFoot
+    },//quidFoot
+
+    footRemodel: function() {
+        console.info('%c footRemodel \u221a','background:blue;color:white;');
+        var target = document.getElementById('newfoot').nextSibling;
+        target.innerHTML = ' <div class="col-xs-12 col-sm-4 col-md-3"> <div class="component"> <div class="responsive-component-title"> <h5 class="hidden-xs">Browse Quidco</h5> <div data-toggle="collapse" data-target="#browse_footer" class="visible-xs"> <h5>Browse Quidco<span class="icon-white-expand-bg"><i class="icon-white-expand"></i></span></h5> </div></div><div id="browse_footer" class="responsive-component-body hidden-xs"> <ul class="list-unstyled"> <li><a href="/browse/" style="outline: 0px;">Browse all</a></li><li><a href="/a-z/" style="outline: 0px;">Browse A-Z</a></li><li><a href="/newest/" style="outline: 0px;">Browse newest</a></li><li><a href="/faster-paying-retailers/" style="outline: 0px;">Browse Faster Paying</a></li></ul> </div></div><div class="component"> <div class="responsive-component-title"> <h5 class="hidden-xs">Useful features</h5> <div data-toggle="collapse" data-target="#hotOffers_footer" class="visible-xs"> <h5>Useful features<span class="icon-white-expand-bg"><i class="icon-white-expand"></i></span></h5> </div></div><div id="hotOffers_footer" class="responsive-component-body1 hidden-xs"> <ul class="list-unstyled"> <li><a href="/quidco-high-street/">Quidco High Street</a></li><li><a href="/clicksnap/">ClickSnap</a></li><li><a href="/quidco-compare/">Quidco Compare</a></li><li><a href="/app/">Quidco mobile app</a></li><li><a href="/free-cashback/">Quidco Opinions</a></li><li><a href="/cashback-reminder/">Quidco Cashback Reminder</a></li><li><a href="/referrals/">Quidco on Facebook</a></li><li><a href="/referrals/">Quidco on Twitter</a></li><li><a href="/referrals/">Refer Quidco</a></li></ul> </div></div></div><div class="col-xs-12 col-sm-4 col-md-3"> <div class="component"> <div class="responsive-component-title"> <h5 class="hidden-xs">Support</h5> <div data-toggle="collapse" data-target="#support_footer" class="visible-xs"> <h5>Support<span class="icon-white-expand-bg"><i class="icon-white-expand"></i></span></h5> </div></div><div id="support_footer" class="responsive-component-body1 hidden-xs"> <ul class="list-unstyled"> <li><a href="/help/">Contact Quidco Support</a></li><li><a href="/new-enquiry/">Create cashback claim</a></li><li><a href="/highest-cashback-guarantee/">Highest Cashback Guarantee</a></li><li><a href="/help/all/">All help</a></li><li><a href="/accessibility/">Accessibility</a></li></ul> </div></div><div class="component"> <div class="responsive-component-title"> <h5 class="hidden-xs">Hot offers</h5> <div data-toggle="collapse" data-target="#usefulFeatures_footer" class="visible-xs"> <h5>Hot offers<span class="icon-white-expand-bg"><i class="icon-white-expand"></i></span></h5> </div></div><div id="usefulFeatures_footer" class="responsive-component-body1 hidden-xs"> <ul class="list-unstyled"> <li><a href="/hot-offers/">Black Friday Deals</a></li><li><a href="/hot-offers/">Hottest</a></li><li><a href="/top-cashback/">Top cashback</a></li><li><a href="/top-voucher-codes/">Top voucher codes</a></li><li><a href="/mobile-in-store-offers/">Mobile & In-store</a></li><li><a href="/free-cashback/">Free cashback</a></li><li><a href="/hidden-gems/">Hidden gems</a></li></ul> </div></div></div><div class="col-xs-12 col-sm-4 col-md-3"> <div class="component"> <div class="responsive-component-title"> <h5 class="hidden-xs">Company</h5> <div data-toggle="collapse" data-target="#company_footer" class="visible-xs"> <h5>Company<span class="icon-white-expand-bg"><i class="icon-white-expand"></i></span></h5> </div></div><div id="company_footer" class="responsive-component-body1 hidden-xs"> <ul class="list-unstyled"> <li><a href="/business/about/" target="_blank">About</a></li><li><a href="/business/" target="_blank">Business</a></li><li><a href="/contact/">Contact</a></li><li><a href="/business/press/" target="_blank">Press</a></li><li><a href="https://www.maplesyrupmedia.com/careers/" target="_blank">Careers</a></li><li><a href="/sitemap/">Site map</a></li><li><a href="/blog/">Blog</a></li><li><a href="/blog/" target="_blank">Tech Blog</a></li></ul> </div></div><div class="component"> <div class="responsive-component-title"> <h5 class="hidden-xs">Policies</h5> <div data-toggle="collapse" data-target="#policies_footer" class="visible-xs"> <h5>Policies<span class="icon-white-expand-bg"><i class="icon-white-expand"></i></span></h5> </div></div><div id="policies_footer" class="responsive-component-body1 hidden-xs"> <ul class="list-unstyled"> <li><a href="/quidco-account-holder-agreement/">Quidco Account Holder Agreement</a> </li><li><a href="/privacy-policy/">Privacy Policy</a></li></ul> </div></div></div><div class="col-xs-12 col-sm-12 col-md-3"> <div class="component footer-social-numbers"> <ul class="list-unstyled"> <li class="member-count"> <div class="icon"> <svg class="social-icon"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-q-ident"></use> </svg> </div><div class="text">7 million members</div></li><li class="twitter-count"> <a href="http://www.twitter.com/quidco"> <div class="icon"> <svg class="social-icon"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-social-twitter"></use> </svg> </div><div class="text">50,925 followers</div></a> </li><li class="facebook-count"> <a href="http://www.facebook.com/quidco"> <div class="icon"> <svg class="social-icon"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-social-facebook"></use> </svg> </div><div class="text">156,896 fans</div></a> </li></ul> </div></div>';
+
+            //website code misaligned(right column) - adding a free fix
+            var freefix = document.querySelectorAll('.related-retailers-list')[2];
+            freefix.style.marginTop = "1.5em";
+
+
+    }//footRemodel
     
 }; 
 
